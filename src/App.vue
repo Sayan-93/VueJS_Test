5b99db7ffbeae33a704060756a3aa29169d9ffcc
Sayan-93 <21f3000735@ds.study.iitm.ac.in>
2025-07-23 16:44:54 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <h1>{{ title }}</h1>
+  <Modal :header="header" :text="text" theme="sale"/>
+</template>
+
+<script>
+import Modal from './components/Modal.vue'
+
+export default {
+  name: 'App',
+  components: { Modal },
+  data() {
+    return{
+      title: 'New Vue project',
+      header: 'Sign up!',
+      text: 'Good luck'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1 {
+  border-bottom: 10px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+</style>
