29c6b59edadcf50c2d431e0ec1da66e123a55c8e
Sayan-93 <21f3000735@ds.study.iitm.ac.in>
2025-07-27 14:02:40 +0530

    Just fixed some line spacing

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -3,13 +3,10 @@
     <div class="backdrop" @click.self="closeModal">
         <div class="modal" :class="{ sale: theme === 'sale' }">
             <slot></slot>
-            
             <slot name="links"></slot>
-            
         </div>
     </div>
-    
-    
+       
 </template>
 
 <script>
